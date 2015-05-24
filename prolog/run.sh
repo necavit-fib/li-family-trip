f7a912a141493308c1c713491adc0645177387f2
David Martínez <gollumdeagol@gmail.com>
2015-05-24 12:26:02 +0200

    removed unnecessary Makefile in favour of a single script

--- prolog/run.sh
+++ prolog/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Compiling solver (make) into 'solve' executable..."
-make
+echo "Compiling solver into 'solve' executable..."
+swipl -O -g solve --stand_alone=true -o solve -c family-trip.pl
 
 echo "Running solver..."
 ./solve
