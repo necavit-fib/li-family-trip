f672b7065d13fb5f1d1eb44135ec1bb016abfd4e
David Martínez <gollumdeagol@gmail.com>
2015-05-29 15:37:15 +0200

    whitespaces and formatting

--- prolog/family-trip.pl
+++ prolog/family-trip.pl
@@ -3,13 +3,13 @@ cities([paris,bangkok,montevideo,windhoek,male,delhi,reunion,lima,banff]).
 
 interests([landscapes,culture,ethnics,gastronomy,sport,relax]).
 
-attractions( paris,     [culture,gastronomy]      ).
+attractions( paris,     [culture,gastronomy]       ).
 attractions( bangkok,   [landscapes,relax,sport]   ).
-attractions( montevideo,[gastronomy,relax]        ).
-attractions( windhoek,  [ethnics,landscapes]         ).
+attractions( montevideo,[gastronomy,relax]         ).
+attractions( windhoek,  [ethnics,landscapes]       ).
 attractions( male,      [landscapes,relax,sport]   ).
-attractions( delhi,     [culture,ethnics]           ).
-attractions( reunion,   [sport,relax,gastronomy] ).
+attractions( delhi,     [culture,ethnics]          ).
+attractions( reunion,   [sport,relax,gastronomy]   ).
 attractions( lima,      [landscapes,sport,culture] ).
 attractions( banff,     [sport,landscapes]         ).
 
