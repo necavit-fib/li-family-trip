230ca48f4e0572d53df8f0e14e79bc3c49b48101
David Martínez <gollumdeagol@gmail.com>
2015-05-30 10:57:59 +0200

    moved some code to a separate folder

--- sat/run.sh
+++ sat/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Compiling solver into 'solve' executable..."
-swipl -O -g main --stand_alone=true -o solve -c family-trip-solver.pl
+swipl -O -g main --stand_alone=true -o solve -c $1
 
 echo "Running solver..."
 ./solve
